53b308b10c5f114b786bb86a80cc846ea3cd5306
Jussif79 <jussif.79@gmail.com>
2025-09-30 09:09:20 +0200

    Add: updateTask function

--- src/main/scala/todo/model/TodoModel.scala
+++ src/main/scala/todo/model/TodoModel.scala
@@ -11,6 +11,23 @@ case class TodoModel(tasks: List[Task], nextId: Int) {
              ): TodoModel = {
     val newTask = Task.create(nextId, title, description, category, deadline)
     TodoModel(newTask :: tasks, nextId + 1)
+  }
 
+  def updateTask(
+                  id: Int,
+                  title: Option[String] = None,
+                  description: Option[String] = None,
+                  category: Option[Category] = None,
+                  deadline: Option[LocalDate] = None,
+                  status: Option[Status] = None
+                ): TodoModel = {
+    val updatedTasks = tasks.map { task =>
+      if (task.id == id) {
+        Task.update(task, title, description, category, deadline, status)
+      } else {
+        task
+      }
+    }
+    TodoModel(updatedTasks, nextId)
   }
 }
