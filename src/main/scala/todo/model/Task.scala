88d27744d361c2bf4ff2b741725ae3bb298a6542
Tenzin Tobatsang <tenzin.tob8@gmail.com>
2025-09-30 08:44:39 +0200

    CHG: category trait for task

--- src/main/scala/todo/model/Task.scala
+++ src/main/scala/todo/model/Task.scala
@@ -1,2 +1,24 @@
 package todo.model
 
+/**
+ * Represents the category of a task
+ */
+sealed trait Category
+object Category {
+  case object Work extends Category
+  case object School extends Category
+  case object Private extends Category
+
+  def fromString(str: String): Option[Category] = str.toLowerCase match {
+    case "work" => Some(Work)
+    case "school" => Some(School)
+    case "private" => Some(Private)
+    case _ => None
+  }
+
+  def toString(category: Category): String = category match {
+    case Work => "work"
+    case School => "school"
+    case Private => "private"
+  }
+}
